59f1a53e76c4c26816404cbf5ef778d0b897c6b0
OlegVazhinsky <oleg_vazhinsky92@mail.ru>
2022-10-16 15:59:25 +0700

    Выполнил задание 10.

---
+++ Task10/Program.cs
@@ -0,0 +1,14 @@
+﻿int GetUserNumber (string message) {
+    Console.Write(message);
+    return int.Parse(Console.ReadLine());
+}
+
+int userNumber = GetUserNumber("Введите трехзначное число = ");
+int answer = 0;
+
+if ((userNumber < 100 && userNumber > -99) || userNumber > 999 || userNumber < -999) Console.WriteLine($"Число {userNumber} не трехзначное.");
+else {
+    if (userNumber > 0) answer = (userNumber % 100) / 10;
+    else answer =  (-userNumber % 100) / 10;
+    Console.WriteLine($"Вторая цифра числа {userNumber} = {answer}");
+}
