7847ce07c6d407c2518e5d160c268caa937fd106
OlegVazhinsky <oleg_vazhinsky92@mail.ru>
2022-10-16 16:15:59 +0700

    Выполнил задание 13.

---
+++ Task13/Program.cs
@@ -0,0 +1,14 @@
+﻿int GetUserNumber (string message) {
+    Console.Write(message);
+    return int.Parse(Console.ReadLine());
+}
+
+int userNumber = GetUserNumber("Введите трех или более значное число = ");
+int answer = 0;
+
+if (userNumber < 100 && userNumber > -100 ) Console.WriteLine($"Число {userNumber} не трех или более значное.");
+else {
+    if (userNumber > 0) answer = (userNumber % 10);
+    else answer =  (-userNumber % 10);
+    Console.WriteLine($"Третья цифра числа {userNumber} = {answer}");
+}
