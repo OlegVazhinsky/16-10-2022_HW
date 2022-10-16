b8bcb6229857964913960c60a8be623842e8af7c
OlegVazhinsky <oleg_vazhinsky92@mail.ru>
2022-10-16 16:22:25 +0700

    Выполнил задание 15.

---
+++ Task15/Program.cs
@@ -0,0 +1,13 @@
+﻿int GetUserNumber (string message) {
+    Console.Write(message);
+    return int.Parse(Console.ReadLine());
+}
+
+int userNumber = GetUserNumber("Введите цифру соответствующую дню недели (от 1 до 7) = ");
+int answer = 0;
+
+if (userNumber < 1 || userNumber > 7 ) Console.WriteLine($"Число {userNumber} не соответствует дню недели.");
+else {
+    if (userNumber > 5 && userNumber < 8) Console.WriteLine($"Цифра {userNumber} соответствует выходному дню.");
+    else Console.WriteLine($"Цифра {userNumber} НЕ соответствует выходному дню.");
+}
